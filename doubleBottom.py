a7753adf654440a8fa0fb8a20a3dbcdc9a7e0e02
Thorsten <thorsten.mueller@js-soft.com>
2022-10-30 18:30:44 +0100

    init commit

---
+++ doubleBottom.py
@@ -0,0 +1,134 @@
+import yfinance as yf
+import yahoo_fin.stock_info as si
+from typing import Callable
+import numpy as np
+import pandas as pd
+
+msft = yf.Ticker("DOW")
+res = msft.history("3mo", "1d")
+#._values holds the data in an array with the following format 
+# [Open, High, Low, Close, Volume, Dividends, Splits]
+closeIndex = 3
+lowIndex = 2
+
+rsiLength = 14
+daysAheadThreshold = 2
+threshold = 1.5
+lowerLowThreshold = 0.99
+
+aPotentailLows = [] #keeps track of the indices of the potential lows
+aLows = [] # the actual lows
+aDoubleBottoms = [] 
+
+#._values will have the latest data on top (at index 0)
+#print(res._values[0][3])
+#Date is store here
+#res.index.date[64]
+
+
+#Stolen from: https://stackoverflow.com/questions/20526414/relative-strength-index-in-python-pandas
+# Define function to calculate the RSI
+def calc_rsi(over: pd.Series, fn_roll: Callable) -> pd.Series:
+    # Get the difference in price from previous step
+    delta = over.diff()
+    # Get rid of the first row, which is NaN since it did not have a previous row to calculate the differences
+    #delta = delta[1:] 
+
+    # Make the positive gains (up) and negative gains (down) Series
+    up, down = delta.clip(lower=0), delta.clip(upper=0).abs()
+
+    roll_up, roll_down = fn_roll(up), fn_roll(down)
+    rs = roll_up / roll_down
+    rsi = 100.0 - (100.0 / (1.0 + rs))
+
+    # Avoid division-by-zero if `roll_down` is zero
+    # This prevents inf and/or nan values.
+    rsi[:] = np.select([roll_down == 0, roll_up == 0, True], [100, 0, rsi])
+    rsi.name = 'rsi'
+
+    # Assert range
+    valid_rsi = rsi[rsiLength - 1:]
+    assert ((0 <= valid_rsi) & (valid_rsi <= 100)).all()
+    # Note: rsi[:length - 1] is excluded from above assertion because it is NaN for SMA.
+
+    return rsi
+
+
+
+def getDoubleBottoms(yfResponse):
+    #set index to the last possible day
+    amntData = len(yfResponse._values) - 1
+    index = 0
+    while index < amntData:
+        # searched the next two candles no lower low
+
+        #criteria 2: it closes lower during the next two days
+            # if true kick it
+        bLowerLow = False
+        daysAhead = 1
+        while daysAhead <= 2:
+            if(yfResponse._values[index + daysAhead][closeIndex] < yfResponse._values[index][closeIndex]):
+                bLowerLow = True
+                break
+
+            if daysAhead == daysAheadThreshold and bLowerLow == False:
+                aPotentailLows.append(index) if index not in aPotentailLows else aPotentailLows
+
+            daysAhead = daysAhead + 1
+            #elif (res._values[indexAhead][lowIndex] < res._values[indexAhead][lowIndex]):
+        #else -> criteria 1: there is a new low
+            #sub-criteria 1: it is more than 1% lower
+                # kick it
+            # save date
+
+        #do something
+        index = index + 1
+
+    #ensure that each potentail low is checked against its past. Hence, it is not an potentail low if it had a lower low the two days before
+    for potentialLowIndex in aPotentailLows:
+        daysBackThreshold = 2
+        daysBack = 1
+        bLowerLow = False
+        while daysBack <= daysBackThreshold:
+
+            if(yfResponse._values[potentialLowIndex - daysBack][closeIndex] < yfResponse._values[potentialLowIndex][closeIndex]):
+                bLowerLow = True
+                break
+
+            if daysBack == daysAheadThreshold and bLowerLow == False:
+                aLows.append(potentialLowIndex) if potentialLowIndex not in aLows else aLows
+
+            daysBack = daysBack + 1        
+
+
+    #first loop goes until the second last entry
+    #second loop is always one entry ahead of the first one. Hence, it goes until the last entry
+    amntLows = len(aLows)
+    firstIndex = 0
+    secondIndex = 0
+
+    while firstIndex < amntLows - 2:
+        secondIndex = firstIndex + 1 # second index is at least one step ahead of first index
+        firstLowIndex = aLows[firstIndex] # ensure to get the index of the actual low
+        
+        while secondIndex < amntLows - 1:
+            secondLowIndex = aLows[secondIndex]
+            if (0.985 < yfResponse._values[firstLowIndex][closeIndex] / yfResponse._values[secondLowIndex][closeIndex] and yfResponse._values[firstLowIndex][closeIndex] / yfResponse._values[secondLowIndex][closeIndex] < 1.015):
+                print ("DB: ", yfResponse.index.date[firstLowIndex], " - ", yfResponse.index.date[secondLowIndex])
+                if (yfResponse.rsi._values[secondLowIndex] / yfResponse.rsi._values[firstLowIndex] > 1.3):
+                    print("RSI Divergence + DB: ", yfResponse.index.date[firstLowIndex], " - ", yfResponse.index.date[secondLowIndex])
+            
+            secondIndex = secondIndex +1
+        
+        firstIndex = firstIndex + 1 
+
+
+
+res.rsi = calc_rsi(res['Close'], lambda s: s.ewm(alpha=1 / rsiLength).mean())
+getDoubleBottoms(res)
+
+# http://theautomatic.net/yahoo_fin-documentation/
+# Loop with each ticker over the script
+print (si.tickers_dow())
+print (si.tickers_sp500())
+print (si.tickers_nasdaq())
---
+++ rsiTest.py
@@ -0,0 +1,62 @@
+import datetime
+from typing import Callable
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as web
+import yfinance as yf
+
+msft = yf.Ticker("DOW")
+data = msft.history("3mo", "1d")
+
+# Window length for moving average
+length = 14
+
+# Dates
+start, end = '2022-01-01', '2022-10-29'
+
+# Get data
+#data = web.DataReader('DOW', 'yahoo', start, end)
+# Get just the adjusted close
+#close = data['Adj Close']
+close = data['Close']
+
+# Define function to calculate the RSI
+def calc_rsi(over: pd.Series, fn_roll: Callable) -> pd.Series:
+    # Get the difference in price from previous step
+    delta = over.diff()
+    # Get rid of the first row, which is NaN since it did not have a previous row to calculate the differences
+    delta = delta[1:] 
+
+    # Make the positive gains (up) and negative gains (down) Series
+    up, down = delta.clip(lower=0), delta.clip(upper=0).abs()
+
+    roll_up, roll_down = fn_roll(up), fn_roll(down)
+    rs = roll_up / roll_down
+    rsi = 100.0 - (100.0 / (1.0 + rs))
+
+    # Avoid division-by-zero if `roll_down` is zero
+    # This prevents inf and/or nan values.
+    rsi[:] = np.select([roll_down == 0, roll_up == 0, True], [100, 0, rsi])
+    rsi.name = 'rsi'
+
+    # Assert range
+    valid_rsi = rsi[length - 1:]
+    assert ((0 <= valid_rsi) & (valid_rsi <= 100)).all()
+    # Note: rsi[:length - 1] is excluded from above assertion because it is NaN for SMA.
+
+    return rsi
+
+# Calculate RSI using MA of choice
+# Reminder: Provide ≥ `1 + length` extra data points!
+rsi_ema = calc_rsi(close, lambda s: s.ewm(span=length).mean())
+rsi_sma = calc_rsi(close, lambda s: s.rolling(length).mean())
+rsi_rma = calc_rsi(close, lambda s: s.ewm(alpha=1 / length).mean())  # Approximates TradingView.
+
+# Compare graphically
+plt.figure(figsize=(8, 6))
+#rsi_ema.plot(), rsi_sma.plot(), 
+rsi_rma.plot()
+#plt.legend(['RSI via EMA/EWMA', 'RSI via SMA', 'RSI via RMA/SMMA/MMA (TradingView)'])
+plt.show()
